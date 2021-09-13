ba31c9c03aacd515337244de164bc785f2448c23
ElijahJClark <89264080+ElijahJClark@users.noreply.github.com>
2021-09-12 21:30:09 -0500

    Add files via upload

---
+++ Assignment#1.py
@@ -0,0 +1,15 @@
+print("Elijah Clark"), print("San Diego,CA")
+#question 2
+problem_1 = (3*4*5)
+print(problem_1)
+#question 3 
+division = (403//3)
+remainder = (403%3)
+print("division=", division)
+print("remainder=", remainder)
+#question 4 
+age=18
+jobTitle='Student'
+print("age:",age), print('Job Title:',jobTitle)
+#question 5 
+print("Could'y find any unicode tables that fits with python")
